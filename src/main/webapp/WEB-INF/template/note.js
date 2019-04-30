c76552ca40261a2b8f8be2445f6146be42ed17d4
baowei <1368183370@qq.com>
2019-04-30 17:21:46 +0800

    为note模块添加了基础页面 添加了富文本编辑器作为记笔记的插件

--- src/main/webapp/WEB-INF/template/note.js
+++ src/main/webapp/WEB-INF/template/note.js
@@ -3,16 +3,25 @@
  */
 var noteTemplate = `
 <el-card shadow="hover" style="margin: 10px 0px">
-    <div></div>
+    <span>笔记时间: {{note.noteDate}}</span>
+    <div v-html="note.noteContent"></div>
+    <my_col_paragraph :col_paragraph="col_paragraph" :index="index"></my_col_paragraph>
 </el-card>
 `
 
 var noteModule = {
     data:function () {
         return{
+            note_content:'content'+this.index,
+            col_paragraph:{
+                sectionName:"第一节",
+                courseName:"测试md",
+                paragraphContent:"图片的描述”功能暂未计划开发，请用“#”代替，如1.1.2.结构框图中图片。图片要**单独**一段。",
+                colCount:100
+            }
         }
     },
-    props:['note'],
+    props:['note','index'],
     template: noteTemplate,
     mounted:function(){
     },
