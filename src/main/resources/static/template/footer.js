2d5d4c1abfb9fb5eb24e8cec4c3389e6a8aa4855
baowei <1368183370@qq.com>
2019-06-20 13:09:01 +0800

    修改了最新的字体库 修改了底部的copyright

--- src/main/resources/static/template/footer.js
+++ src/main/resources/static/template/footer.js
@@ -5,7 +5,7 @@ var footerTemplate = `
 <el-footer>
         <el-row type="flex" class="row-bg" justify="center">
             <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="4"><div class="grid-content bg-purple-light" style="color: gray;text-align: center;line-height: 36px;">&copy;山软智库2017</div></el-col>
             <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
         </el-row>
 </el-footer>
