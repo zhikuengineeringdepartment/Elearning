144d1b38cdb5cfff59b6f679f6212f65dec13113
wuchvi <wuchvi@gmail.com>
2019-11-23 14:28:30 +0800

    修改文件名

---
+++ src/pages/forum/overview/Post.vue
@@ -0,0 +1,70 @@
+<!--帖子组件-->
+<template>
+  <el-col :xs="24" :sm="20" :md="16" :lg="14" :xl="12">
+    <div class="post">
+      <div class="title">{{ title }}</div>
+      <el-collapse>
+        <el-collapse-item :title="content">
+          <div>
+            与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+      <div class="user-info">
+        <el-avatar size="small" :src="circleUrl"></el-avatar>
+        <span>bobking</span>
+      </div>
+    </div>
+  </el-col>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  data() {
+    return {
+      title: "100 美元一行代码，开源软件到底咋赚钱",
+      content: "  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)...",
+      circleUrl:
+        "https://imgsa.baidu.com/forum/w%3D580%3B/sign=44a183bdbdfb43161a1f7a72109f4723/b7003af33a87e9504ce6101a1f385343faf2b4d7.jpg"
+    };
+  }
+};
+</script>
+
+<style scoped lang="less">
+.post {
+  padding: 20px;
+  text-align: left;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.post:hover {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.title {
+  font-size: 1.8vmin;
+  font-weight: 700;
+}
+
+.user-info {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+</style>
+
+<style>
+.el-collapse,
+.el-collapse-item__wrap,
+.el-collapse .el-collapse-item__header {
+  border: none !important;
+}
+
+.el-collapse-item__content {
+  padding: 0 !important;
+}
+</style>
---
+++ src/pages/forum/overview/SearchBar.vue
@@ -0,0 +1,48 @@
+<!--帖子组件-->
+<template>
+  <el-col :xs="24" :sm="20" :md="16" :lg="14" :xl="12">
+    <el-select
+      v-model="course"
+      clearable
+      placeholder="请选择课程"
+      class="input"
+    >
+      <el-option
+        v-for="item in courselist"
+        :key="item"
+        :label="item"
+        :value="item"
+      >
+      </el-option>
+    </el-select>
+
+    <el-input
+      placeholder="请输入搜索内容"
+      v-model="keyword"
+      class="input-with-select input"
+    >
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+  </el-col>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      courselist: ["选项1", "黄金糕", "选项2", "双皮奶"],
+      course: "",
+      keyword: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.input {
+  width: 40%;
+  min-width: 200px;
+  margin: 0 5% 3vmin 5%;
+}
+</style>
