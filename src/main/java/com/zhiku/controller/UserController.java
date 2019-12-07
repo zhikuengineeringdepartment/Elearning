df14e8995ac18388ffd7b3cbeecb700a764cdf9e
namelessdeveloper <1924019307@qq.com>
2019-12-07 22:17:21 +0800

    修改管理员部分bug

--- src/main/java/com/zhiku/controller/UserController.java
+++ src/main/java/com/zhiku/controller/UserController.java
@@ -138,11 +138,13 @@ public class UserController {
                         cookie.setMaxAge(3*60*60);
                         cookie.setPath("/");
                         response.addCookie(cookie);
-                        //签发管理员token
-                        Cookie cookie2 = new Cookie("tokena",JWTUtil.signToken(user));
-                        cookie2.setMaxAge(3*60*60);
-                        cookie2.setPath("/");
-                        response.addCookie(cookie2);
+                        if(user.getUserAuth().equals( "a" )){
+                            //签发管理员token
+                            Cookie cookie2 = new Cookie("tokena",JWTUtil.signToken(user));
+                            cookie2.setMaxAge(3*60*60);
+                            cookie2.setPath("/");
+                            response.addCookie(cookie2);
+                        }
                         responseData.putDataValue("userIcon",user.getUserAvatar());
                     }catch (Exception e){
                         e.printStackTrace();
