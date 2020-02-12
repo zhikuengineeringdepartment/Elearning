3fdfaf937d37beaa9c343ef70572ce1af541c8e5
ZYN0315 <1456485043@qq.com>
2020-02-12 18:00:25 +0800

    完成增加评论和删除评论(未测试)

--- src/main/java/com/zhiku/controller/PostReplyController.java
+++ src/main/java/com/zhiku/controller/PostReplyController.java
@@ -2,8 +2,11 @@ package com.zhiku.controller;
 
 import com.zhiku.entity.User;
 import com.zhiku.entity.mysql.Report;
+import com.zhiku.service.PostReplyService;
+import com.zhiku.service.PostService;
 import com.zhiku.service.ReportService;
 import com.zhiku.util.ResponseData;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,22 +22,27 @@ import java.util.Date;
 public class PostReplyController {
 
     @Autowired
-    private ReportService reportService;
+    private PostReplyService postReplyService;
 
+    @Autowired
+    private ReportService reportService;
 
     /**
     * 一级评论接口
     * 传入参数:
-    * 1.帖子id
-    * 2.回复者id
-    * 3.回复内容
+    * @param pId 1.帖子id
+    * @param authorId 2.回复者id
+    * @param replyContent 3.回复内容
     * */
     @RequestMapping("/first")
-    public ResponseData firstReply(@RequestParam("rId") int rId,@RequestParam("authorId") int authorId,
-                                   @RequestParam("replyContent") String replyContent){
-        return null;
+    public ResponseData firstReply(@RequestParam("pId") ObjectId pId, @RequestParam("authorId") int authorId,
+                                   @RequestParam("replyContent") String replyContent) {
+        if (pId == null || authorId < 0 || replyContent == "") {
+            return ResponseData.badRequest();
+        } else {
+            return postReplyService.insert(pId, authorId, replyContent);
+        }
     }
-
     /**
     * 多级评论接口
     * 传入参数:
@@ -44,63 +52,90 @@ public class PostReplyController {
     * 4.回复内容
     * */
     @RequestMapping("/second")
-    public ResponseData secondReply(@RequestParam("rId") int rId,@RequestParam("authorId") int authorId,
+    public ResponseData secondReply(@RequestParam("pId") ObjectId pId,@RequestParam("authorId") int authorId,
                                     @RequestParam("replyContent") String replyContent,@RequestParam("repliedAuthor") int repliedAuthor){
-        return null;
+        if (pId == null || authorId < 0 || repliedAuthor < 0 || replyContent == "") {
+            return ResponseData.badRequest();
+        }
+        else {
+            return postReplyService.insert(pId,authorId,repliedAuthor,replyContent);
+        }
     }
 
     /**
      * 评论点赞接口
      * 传入参数:
-     * 1.评论类型。1表示一级评论，2表示多级评论
-     * 2.评论id
-     * 3.点赞者id
+     * @param type 1.评论类型。1表示一级评论，2表示多级评论
+     * @param rId 2.评论id，
+     * @param rId2 如果为多级评论则还有一个参数
+     * @param agreeId 3.点赞者id
      * */
     @RequestMapping("/agree")
-    public ResponseData agreeReply(@RequestParam("type") int type,@RequestParam("rId") int rId,
-                                   @RequestParam("agreeId") int agreeId){
+    public ResponseData agreeReply(@RequestParam("type") int type,@RequestParam("rId") ObjectId rId,
+                                   @RequestParam("rId2") ObjectId rId2,@RequestParam("agreeId") int agreeId){
         return null;
     }
 
     /**
      * 评论点赞取消接口
      * 传入参数：
-     * 1.评论类型
-     * 2.评论id
-     * 3.点赞者id
+     * @param type 1.评论类型
+     * @param rId 2.评论id
+     * @param rId2 如果是二级评论，则有两个
      * 返回情况
      * 未找到点赞者
      * 找到并取消
      * */
     @RequestMapping("/cancelagree")
-    public ResponseData cancelAgreeReply(@RequestParam("type") int type,@RequestParam("rId") int rId,
-                                         @RequestParam("agreeId") int agreeId){
+    public ResponseData cancelAgreeReply(@RequestParam("type") int type,@RequestParam("rId") ObjectId rId,
+                                         @RequestParam("rId2") ObjectId rId2,@RequestParam("cancelId") int cancelId){
         return null;
     }
 
     /**
      * 删除评论接口
      * 传入参数:
-     * 1.评论类型
-     * 2.评论id
+     * @param type 1.删除类型，1表示删除一篇文章所有评论，2表示删除单条评论
+     * @param pId 2.文章id
+     * @param rId 3.评论id
+     * @param rId2 如果是二级评论，则有两个
      * 返回情况:
      * 评论不存在
      * 删除成功
      * */
     @RequestMapping("/deletereply")
-    public ResponseData deleteReply(@RequestParam("type") int type,@RequestParam("rId") int rId){
-        return null;
+    public ResponseData deleteReply(@RequestParam("type") int type, @RequestParam("pId") ObjectId pId,
+                                    @RequestParam("rId") String rId, @RequestParam("rId2") String rId2){
+        if (type==1){
+            return postReplyService.delete(pId);
+        }
+        else if (type==2){
+            if (rId == ""){
+                return ResponseData.badRequest();
+            }
+            else {
+                if (rId2 == ""){
+                    return postReplyService.delete(pId,rId);
+                }
+                else {
+                    return postReplyService.delete(pId,rId,rId2);
+                }
+            }
+        }else {
+            return ResponseData.badRequest();
+        }
     }
 
     /**
      * 查询评论接口
      * 传入参数:
-     * 1.帖子id
+     * @param pId
+     *
      * 返回数据:
      * postfirstreply对象数组
      * */
     @RequestMapping("/getreply")
-    public ResponseData getReply(@RequestParam("rId") int rId){
+    public ResponseData getReply(@RequestParam("pId") int pId){
         return null;
     }
 
